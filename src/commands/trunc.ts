bb2c49d534b49169de4cf4c397afeb832654896b
orfao <marco.orfao@etu.u-pec.fr>
2024-11-19 14:59:17 +0100

    added : Interaction listener

--- src/commands/trunc.ts
+++ src/commands/trunc.ts
@@ -1,8 +1,10 @@
 import { SlashCommandBuilder } from "discord.js";
 import Command from "../classes/Command";
 
-export default new Command(new SlashCommandBuilder().setName("trunc").setDescription("fhdushlkjw"), (slash) => {
 
+export default new Command(new SlashCommandBuilder().setName("trunc").setDescription("Choose a number between 1 and 100 and i will trunc this amount of messages for you !"),
+ (slash) => {-
+
+    slash.reply(`Truncating messages...`);
     
-
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Events } from "discord.js";
 import Client from "./classes/Client";
 
 const client = new Client();
@@ -5,4 +6,9 @@ client.start(Bun.env.TOKEN!);
 
 client.on('ready', () => {
   console.log('Bot is ready');
+});
+
+client.on(Events.InteractionCreate, interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  client.commands.get(interaction.commandName)?.exec(interaction);
 });
