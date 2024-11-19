02f11003a58128e411feada7d191304f03d4a52b
orfao <marco.orfao@etu.u-pec.fr>
2024-11-19 20:06:51 +0100

    added : command "Trunc"

--- src/commands/trunc.ts
+++ src/commands/trunc.ts
@@ -1,10 +1,37 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+  TextChannel,
+} from "discord.js";
 import Command from "../classes/Command";
 
-
-export default new Command(new SlashCommandBuilder().setName("trunc").setDescription("Choose a number between 1 and 100 and i will trunc this amount of messages for you !"),
- (slash) => {-
-
-    slash.reply(`Truncating messages...`);
-    
-})
+export default new Command(
+  new SlashCommandBuilder()
+    .setName("trunc")
+    .setDescription(
+      "Choose a number between 1 and 100 and i will trunc this amount of messages for you !"
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("Amount of messages to trunc")
+        .setRequired(true)
+    ) as SlashCommandBuilder,
+  async (slash) => {
+    try {
+      const amount = slash.options.getInteger("amount", true);
+      await (slash.channel as TextChannel).bulkDelete(amount);
+      await slash.reply({
+        content: `Truncating ${amount} messages...`,
+        ephemeral: true,
+      });
+    } catch (error) {
+      slash.reply({
+        content:
+          "Message truncation failed ! Messages must be younger than 14 days !",
+        ephemeral: true,
+      });
+    }
+  }
+);
